254f374c5fb6efb4fdd1cd47b444ce0a7b2a9d01
putiwen <pw1246@nyu.edu>
2023-03-21 04:13:39 -0400

    re-organize

--- init_decode.m
+++ init_decode.m
@@ -36,19 +36,20 @@ for iRun = 1:numel(runNum)
         input = sprintf('%s/derivatives/fmriprep/%s/%s/func/%s_%s_task-%s_run-%s_space-fsnative_hemi-%s_bold.func.gii',param.bids,param.sub,param.ses,param.sub,param.ses,param.task,num2str(iRun),hemi{iH});
         output = sprintf('%s/derivatives/fmriprep/%s/%s/func/%s_%s_task-%s_run-%s_space-fsnative_hemi-%s_bold.func.mgh',param.bids,param.sub,param.ses,param.sub,param.ses,param.task,num2str(iRun),hemi{iH});
         if ~exist(output)
-        system(['mri_convert ' input ' ' output]);
+            system(['mri_convert ' input ' ' output]);
         end
     end
 end
 datafiles = load_data(param.bids,param.task,'fsnative','.mgh',param.sub,param.ses,1:10);
 %%
+nuisance = cell(runNum,1);
 for iRun = 1:numel(runNum)
     
     f1 = dir(sprintf('%s/%s/%s/*run-%s_*',param.drop,param.sub,param.ses,num2str(iRun)));
     f1 = load([f1.folder '/' f1.name]);
     dsCon{iRun} = f1.pa.dsCon;
     dsTrial{iRun} = f1.pa.dsTrial;
-    design{iRun} = f1.pa.design;    
+    design{iRun} = f1.pa.design;
     if isempty(datafiles) % simulate data if no data file
         neuralActivity = zeros(trNum,1);
         neuralActivity(ind) = (f1.pa.design(:,3)-1)*10+f1.pa.design(:,5);
@@ -63,20 +64,29 @@ for iRun = 1:numel(runNum)
         noise = noiseSD * randn(size(data));
         datafiles{iRun} = data + noise + repmat(driftRate * (1:trNum)',1,size(data,2));
     end
+    
+
+    tmpregressor = [param.bids '/derivatives/fmriprep/' param.sub '/' param.ses '/func/' param.sub '_' param.ses '_task-' param.task '_run-' num2str(iRun) '_desc-confounds_timeseries'];
+    if ~exist([tmpregressor '.csv'],'file')
+        system([mv [tmpregressor '.tsv'] [tmpregressor '.csv']])
+    end
+    tmpregressor = readtable(tmpregressor);
+    nuisance(iRun) = [tmpregressor.global_signal str2double(tmpregressor.global_signal_derivative1) tmpregressor.csf tmpregressor.global_signal_power2 tmpregressor.white_matter tmpregressor.trans_x str2double(tmpregressor.trans_x_derivative1) tmpregressor.trans_x_power2 tmpregressor.trans_y str2double(tmpregressor.trans_y_derivative1) tmpregressor.trans_y_power2 tmpregressor.trans_z str2double(tmpregressor.trans_z_derivative1) tmpregressor.trans_z_power2 tmpregressor.rot_x str2double(tmpregressor.rot_x_derivative1) tmpregressor.rot_x_power2 tmpregressor.rot_y str2double(tmpregressor.rot_y_derivative1) tmpregressor.rot_y_power2 tmpregressor.rot_z str2double(tmpregressor.rot_z_derivative1) tmpregressor.rot_z_power2 tmpregressor.a_comp_cor_00 tmpregressor.t_comp_cor_00];  
+    
 end
 label = cat(1,design{:});
 label(label(:,5)==5,:)=[];
 label(:,end+1) = repelem(1:size(datafiles,2),size(label,1)/size(datafiles,2))';
-    %% load ROIs
-    param.roi = {'V1','V3','V3A','V7','V4t','MT','MST','FST'};
-    roimask = cell(1,numel(param.roi));
-    for i = 1:numel(param.roi)
-        [tmpl, ~, ~] = cvnroimask(param.sub,'lh','Glasser2016',param.roi{i},[],[]);
-        [tmpr,~,~] = cvnroimask(param.sub,'rh','Glasser2016',param.roi{i},[],[]);
-        roimask{i} = [tmpl{:};tmpr{:}];
-    end
-    
-%%    
+%% load ROIs
+param.roi = {'V1','V3','V3A','V7','V4t','MT','MST','FST'};
+roimask = cell(1,numel(param.roi));
+for i = 1:numel(param.roi)
+    [tmpl, ~, ~] = cvnroimask(param.sub,'lh','Glasser2016',param.roi{i},[],[]);
+    [tmpr,~,~] = cvnroimask(param.sub,'rh','Glasser2016',param.roi{i},[],[]);
+    roimask{i} = [tmpl{:};tmpr{:}];
+end
+
+%%
 R2 = [];
 %load('/Users/pw1246/Documents/GitHub/CueIntegration2023_analysis/GLMSingle/TYPED_FITHRF_GLMDENOISE_RR.mat', 'R2');
 
